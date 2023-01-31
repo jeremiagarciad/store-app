4f1770c8435ec5ee35625406bc173d48da9292c8
Jeremia Garcia <softwareengeneer421@gmail.com>
2023-01-31 12:57:16 -0400

    code base

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+       <Text style={{
+        fontSize: 28,
+        color: "white"
+      }}>Software Engineer...</Text>
+      <Text style={{
+        fontSize: 28,
+        color: "white"
+      }}>Daily UI Challenge! 100 Days</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D61355',
     alignItems: 'center',
     justifyContent: 'center',
   },
