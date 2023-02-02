e1221a895a96a0ca6c0b46b8d46343d6c46a9642
Jeremia Garcia <softwareengeneer421@gmail.com>
2023-02-02 15:05:16 -0400

    some data

--- App.tsx
+++ App.tsx
@@ -1,39 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { timeElapsed } from "./utils";
+import { useFonts } from "expo-font";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Navigation from "./navigation";
+
+import fonts from "./config/index";
 
 const NEW_DATE = new Date();
-const TIMEELAPSED = 90;
+const TIMEELAPSED = 11000;
+// {timeElapsed(NEW_DATE, TIMEELAPSED)}
 
 export default function App() {
+  const [fontsLoaded] = useFonts(fonts);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 28,
-          color: "white",
-        }}
-      >
-        Software Engineer...
-      </Text>
-      <Text
-        style={{
-          fontSize: 28,
-          color: "white",
-        }}
-      >
-        {timeElapsed(NEW_DATE, TIMEELAPSED)}
-      </Text>
+    <SafeAreaProvider>
+      <Navigation />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#D61355",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
